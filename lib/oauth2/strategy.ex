622ec1fab80900307ba71a74fcd5c4329bade29a
Sonny Scroggin <scrogson@gmail.com>
2014-08-27 02:05:25 -0500

    Base Strategy

---
+++ lib/oauth2/strategy.ex
@@ -0,0 +1,15 @@
+defmodule OAuth2.Strategy do
+
+  alias OAuth2.Client
+
+  defmacro __using__(_opts) do
+    quote do
+      alias OAuth2.Client
+
+      def init(client) do
+        client = Dict.merge(client, [strategy: __MODULE__])
+        struct(Client, client)
+      end
+    end
+  end
+end
