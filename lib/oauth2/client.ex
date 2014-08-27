78fe83210b45fe03b37138ebebf046ea70497d86
Sonny Scroggin <scrogson@gmail.com>
2014-08-27 02:05:08 -0500

    pivot

--- lib/oauth2/client.ex
+++ lib/oauth2/client.ex
@@ -1,40 +1,23 @@
 defmodule OAuth2.Client do
+  @moduledoc """
+  Instantiate a new OAuth 2.0 client using the Client ID and Client Secret
+  registered to your application.
+  """
+
   use HTTPoison.Base
+  require Logger
 
   alias OAuth2.Client
 
-  defstruct grant_type: nil, auth_url: nil, access_token: nil,
-            refresh_token: nil, id: nil, secret: nil, scope: nil
+  defstruct [
+    client_id: nil,
+    client_secret: nil,
+    strategy: nil,
+    host: "",
+    authorize_url: "/oauth/authorize",
+    token_url: "/oauth/token"
+  ]
 
-  def get_access_token(type, url, id, secret, scope \\ :undefined) do
-    %Client{grant_type: type, auth_url: url, id: id, secret: secret, scope: scope}
-    |> do_get_access_token
-  end
-  defp do_get_access_token(%Client{grant_type: "password"} = client) do
-    payload  = request_payload(client)
-    response = post(client.auth_url, payload)
-    IO.inspect response
-  end
-  defp do_get_access_token(%Client{grant_type: "client_credentials"} = client) do
-    payload  = request_payload(client)
-    response = post(client.auth_url, payload)
-    IO.inspect response
-  end
-
-  defp request_payload(%Client{grant_type: "password", scope: :undefined} = client) do
-    [ grant_type: client.grant_type,
-      username:   client.username,
-      password:   client.password ]
-  end
-  defp request_payload(%Client{grant_type: "password"}) do
-    [ grant_type: client.grant_type,
-      username:   client.username,
-      password:   client.password,
-      scope:      client.scope ]
-  end
-  defp request_payload(%Client{grant_type: "client_credentials"} = client) do
-    [ grant_type: client.grant_type,
-      username:   client.username,
-      password:   client.password ]
+  def get_token(client, params \\ %{}, opts \\ %{}) do
   end
 end
