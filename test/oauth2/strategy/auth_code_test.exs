62ea6814e465ed8a122c97150254719f240fdbc3
Sonny Scroggin <scrogson@gmail.com>
2014-10-13 13:46:46 -0500

    Update AuthCode strategy

--- test/oauth2/strategy/auth_code_test.exs
+++ test/oauth2/strategy/auth_code_test.exs
@@ -1,22 +1,41 @@
 defmodule OAuth2.Strategy.AuthCodeTest do
   use ExUnit.Case, async: true
 
+  alias OAuth2.Strategy
   alias OAuth2.Strategy.AuthCode
 
+  @redirect_uri "https://example.com/auth/callback"
+  @encoded_redirect_uri URI.encode_query(%{redirect_uri: @redirect_uri})
+
+  @opts %{
+    client_id: "client_id",
+    client_secret: "secret",
+    site: "https://auth.example.com",
+  }
+
   test "new" do
-    opts = %{
-      client_id: "client_id",
-      client_secret: "secret",
-      site: "https://auth.example.com"
-    }
-    client = AuthCode.new(opts)
-    assert client.__struct__    == OAuth2.Client
-    assert client.strategy      == AuthCode
-    assert client.client_id     == "client_id"
-    assert client.client_secret == "secret"
-    assert client.site          == "https://auth.example.com"
-    assert client.authorize_url == "/oauth/authorize"
-    assert client.token_url     == "/oauth/token"
-    assert client.redirect_uri  == nil
+    strategy = AuthCode.new(@opts)
+    assert strategy.client_id     == "client_id"
+    assert strategy.client_secret == "secret"
+    assert strategy.site          == "https://auth.example.com"
+    assert strategy.authorize_url == "/oauth/authorize"
+    assert strategy.token_url     == "/oauth/token"
+    assert strategy.token_method  == :post
+    assert strategy.params        == %{}
+    assert strategy.headers       == []
+  end
+
+  test "authorize_url" do
+    strategy = AuthCode.new(@opts)
+    params = %{redirect_uri: @redirect_uri}
+    url = OAuth2.authorize_url(strategy, params)
+    assert url == "https://auth.example.com/oauth/authorize?client_id=client_id&#{@encoded_redirect_uri}&response_type=code"
+  end
+
+  test "token_url" do
+    strategy = AuthCode.new(@opts)
+    params = %{redirect_uri: @redirect_uri, code: "abc1234"}
+    url = OAuth2.token_url(strategy, params)
+    assert url == "https://auth.example.com/oauth/token?client_id=client_id&client_secret=secret&code=abc1234&grant_type=authorization_code&#{@encoded_redirect_uri}"
   end
 end
