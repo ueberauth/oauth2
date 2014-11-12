de40ab340059fe26c33eb38df3972959eaa4c9f5
Sonny Scroggin <scrogson@gmail.com>
2014-11-12 14:46:04 -0600

    get_token

--- lib/oauth2.ex
+++ lib/oauth2.ex
@@ -24,5 +24,35 @@ defmodule OAuth2 do
     |> Strategy.to_url(:token_url)
   end
 
+  @doc """
+  Initializes an AccessToken by making a request to the token endpoint.
+
+  Returns an `AccessToken` struct that can then be used to access the resource API.
+
+  ## Arguments
+
+  * `strategy` - a struct of the strategy in use.
+  * `params`   - a map of additional request parameters.
+  * `opts`     - a keyword list of opts used for the request and token.
+  """
+  def get_token(strategy, params \\ %{}, opts \\ [])
+
+  def get_token(%{token_method: :post} = strategy, params, opts) do
+    {headers, body} = Map.pop(params, :headers, [])
+    case Request.post(token_url(strategy), body, post_headers(headers), opts) do
+      {:ok, response}  -> {:ok, AccessToken.new(response.body, strategy, opts)}
+      {:error, reason} -> {:error, %Error{reason: reason}}
+    end
+  end
+  def get_token(strategy, params, opts) do
+    case Request.get(token_url(strategy, params), opts) do
+      {:ok, response}  -> {:ok, AccessToken.new(response.body, strategy, opts)}
+      {:error, reason} -> {:error, %Error{reason: reason}}
+    end
+  end
+
+  def post_headers(headers) do
+    [{"Content-Type", "application/x-www-form-urlencoded"} | headers]
   end
 end
+
