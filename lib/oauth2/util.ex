cab7720e30b0cf90fa8c7d8cc53cb0460ad3424f
Sonny Scroggin <scrogson@gmail.com>
2014-11-12 14:46:47 -0600

    content_type

--- lib/oauth2/util.ex
+++ lib/oauth2/util.ex
@@ -1,6 +1,21 @@
 defmodule OAuth2.Util do
+
   def unix_now do
     {mega, sec, _micro} = :erlang.now
     (mega * 1_000_000) + sec
+  end
+
+  def content_type(headers) do
+    case List.keyfind(headers, "Content-Type", 0) do
+      {"Content-Type", content_type} ->
+        case Plug.Conn.Utils.content_type(content_type) do
+          {:ok, type, subtype, _headers} ->
+            type <> "/" <> subtype
+          :error ->
+            "application/json"
+        end
+      nil ->
+        "application/json"
+    end
   end
 end
