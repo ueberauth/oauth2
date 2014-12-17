6f560510cb9bc32ea6c3a14dc7020b9b1abe6a2e
Sonny Scroggin <scrogson@gmail.com>
2014-12-17 01:14:52 -0600

    Prep mix.exs for hex release

--- mix.exs
+++ mix.exs
@@ -1,22 +1,14 @@
 defmodule OAuth2.Mixfile do
   use Mix.Project
 
-  @description """
-  Elixir OAuth2 Client
-  """
-
   def project do
     [
       app: :oauth2,
       version: "0.0.1",
       elixir: "~> 1.0",
       deps: deps,
-      description: @description,
-      package: [
-        contributors: ["Sonny Scroggin", "Nate West"],
-        licence: "MIT",
-        links: [github: "https://github.com/scrogson/oauth2"]
-      ]
+      package: package,
+      description: "An Elixir OAuth 2.0 Client Library"
     ]
   end
 
@@ -26,11 +18,19 @@ defmodule OAuth2.Mixfile do
 
   defp deps do
     [
-      {:hackney,      "~> 0.14.1"},
-      {:httpoison,    "~> 0.5.0"},
-      {:poison,       "~> 1.2.0"},
-      {:cowboy,       "~> 1.0", optional: true},
-      {:plug,         "~> 0.9.0"},
+      {:hackney, "~> 0.14.1"},
+      {:httpoison, "~> 0.5.0"},
+      {:poison, "~> 1.2.0"},
+      {:cowboy, "~> 1.0", only: :test},
+      {:plug, "~> 0.9.0"},
+    ]
+  end
+
+  defp package do
+    [
+      contributors: ["Sonny Scroggin"],
+      licence: "MIT",
+      links: %{github: "https://github.com/scrogson/oauth2"}
     ]
   end
 end
