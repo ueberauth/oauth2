67a65fd62db5e47c6734b48e11ebaaa877bb9049
Sonny Scroggin <scrogson@gmail.com>
2014-12-24 17:27:44 -0600

    Update ClientCredentials implementation

--- lib/oauth2/strategy/client_credentials.ex
+++ lib/oauth2/strategy/client_credentials.ex
@@ -16,21 +16,28 @@ defmodule OAuth2.Strategy.ClientCredentials do
   @doc """
   Retrieve an access token given the specified strategy.
   """
-  def get_token(strategy, params \\ %{}, opts \\ %{}) do
-    {auth_scheme, opts} = Map.pop(opts, :auth_scheme, "auth_header")
+  def get_token(strategy, params \\ %{}, opts \\ []) do
+    {auth_scheme, opts} = Dict.pop(opts, :auth_scheme, "auth_header")
     params =
       %{grant_type: "client_credentials"}
       |> Map.merge(auth_scheme(auth_scheme, strategy))
       |> Map.merge(params)
 
-    OAuth.get_token(strategy, params, Map.merge(opts, %{refresh_token: nil}))
+    OAuth2.get_token(strategy, params, opts)
+  end
+
+  def get_token!(strategy, params \\ %{}, opts \\ []) do
+    case get_token(strategy, params, opts) do
+      {:ok, token} -> token
+      {:error, error} -> raise error
+    end
   end
 
   @doc """
   Returns the Authorization header value for Basic Authentication.
   """
   def auth_header(%{client_id: id, client_secret: secret}) do
-    %{headers: %{"Authorization" => "Basic " <> Base.encode64(id <> ":" <> secret)}}
+    %{headers: [{"Authorization", "Basic " <> Base.encode64(id <> ":" <> secret)}]}
   end
 
   defp auth_scheme("auth_header", strategy),  do: auth_header(strategy)
