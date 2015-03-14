4d48ab7a43b0066d56d80edcd4503b51c25abe23
Sonny Scroggin <scrogson@gmail.com>
2015-03-14 17:51:30 -0500

    Bump version to 0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OAuth2.Mixfile do
   def project do
     [
       app: :oauth2,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.0",
       deps: deps,
       package: package,
