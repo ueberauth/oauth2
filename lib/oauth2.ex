b0cc09a96a39c3143e4621d0c5ed27d13e36aa01
Sonny Scroggin <scrogson@gmail.com>
2015-04-12 15:02:42 -0500

    Doc updates

--- lib/oauth2.ex
+++ lib/oauth2.ex
@@ -9,6 +9,48 @@ defmodule OAuth2 do
   behalf of a resource owner by orchestrating an approval interaction
   between the resource owner and the HTTP service, or by allowing the
   third-party application to obtain access on its own behalf.
+
+  ## API
+
+  Current implemented strategies:
+
+  - Authorization Code
+  - Password
+  - Client Credentials
+
+  #### Authorization Code Flow (AuthCode Strategy)
+
+  Initialize a client with your client_id, client_secret, and site.
+
+      client = OAuth2.new([
+        strategy: OAuth2.Strategy.AuthCode, # default strategy is AuthCode
+        client_id: "client_id",
+        client_secret: "abc123",
+        site: "https://auth.example.com",
+        redirect_uri: "https://example.com/auth/callback"
+      ])
+
+  Generate the authorization URL and redirect the user to the provider.
+
+      OAuth2.Client.authorize_url(client)
+      # => "https://auth.example.com/oauth/authorize?client_id=client_id&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%2Fcallback&response_type=code"
+
+  Use the authorization code returned from the provider to obtain an access token.
+
+      token = OAuth2.Client.get_token!(client, code: "someauthcode")
+
+  You can also use `OAuth2.Client.put_param/3` to update the client's `params` field.
+
+  Example:
+
+      token =
+        client
+        |> OAuth2.Client.put_param(:code, "someauthcode")
+        |> OAuth2.Client.get_token!()
+
+  Use the access token to make a request for resources
+
+      resource = OAuth2.AccessToken.get!(token, "/api/resource")
   """
 
   @type opts    :: Keyword.t
--- lib/oauth2/strategy.ex
+++ lib/oauth2/strategy.ex
@@ -1,4 +1,67 @@
 defmodule OAuth2.Strategy do
+  @moduledoc ~S"""
+  The OAuth2 strategy specification.
+
+  This module defines the required callbacks for all strategies.
+
+  ## Examples
+
+  Here's an example strategy for authenticating with GitHub.
+
+      defmodule GitHub do
+        use OAuth2.Strategy
+
+        # Public API
+
+        def new do
+          OAuth2.new([
+            strategy: __MODULE__,
+            client_id: "abc123",
+            client_secret: "abcdefg",
+            redirect_uri: "http://myapp.com/auth/callback",
+            site: "https://api.github.com",
+            authorize_url: "https://github.com/login/oauth/authorize",
+            token_url: "https://github.com/login/oauth/access_token"
+          ])
+        end
+
+        def authorize_url!(params \\ []) do
+          new()
+          |> put_param(:scope, "user,public_repo")
+          |> OAuth2.Client.authorize_url!(params)
+        end
+
+        def get_token!(params \\ [], headers \\ []) do
+          OAuth2.Client.get_token!(new(), params, headers)
+        end
+
+        # Strategy Callbacks
+
+        def authorize_url(client, params) do
+          OAuth2.Strategy.AuthCode.authorize_url(client, params)
+        end
+
+        def get_token(client, params, headers) do
+          client
+          |> put_header("Accept", "application/json")
+          |> OAuth2.Strategy.AuthCode.get_token(params, headers)
+        end
+      end
+
+  ## Usage
+
+  Generate the authorize URL and redirect the client for authorization.
+
+      GitHub.authorize_url!
+
+  Capture the `code` in your callback route on your server and use it to obtain an access token.
+
+      token = GitHub.get_token!(code: code)
+
+  Use the access token to access desired resources.
+
+      user = OAuth2.AccessToken.get!(token, "/user")
+  """
   use Behaviour
 
   alias OAuth2.Client
@@ -13,3 +76,4 @@ defmodule OAuth2.Strategy do
     end
   end
 end
+
