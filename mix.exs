2b171205d570296d72ab7328365aef07af2ec10b
Sonny Scroggin <scrogson@gmail.com>
2015-04-18 17:51:55 -0500

    Version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OAuth2.Mixfile do
   def project do
     [
       app: :oauth2,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.0",
       deps: deps,
       package: package,
