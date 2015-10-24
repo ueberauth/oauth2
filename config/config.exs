f763ba6661af9ccfd9597fc7f5c015ed4ae7944c
Sonny Scroggin <scrogson@gmail.com>
2015-10-24 15:55:12 -0500

    just use config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,16 @@
 use Mix.Config
 
-import_config "#{Mix.env}.exs"
+provider_router_port = 4999
+consumer_router_port = 4998
+
+config :oauth2,
+  client_id: "client_id",
+  client_secret: "client_secret",
+  site: "http://localhost:#{provider_router_port}",
+  redirect_uri: "http://localhost:#{consumer_router_port}/auth/callback"
+
+config :oauth2, ProviderRouter,
+  port: provider_router_port
+
+config :oauth2, ConsumerRouter,
+  port: consumer_router_port
