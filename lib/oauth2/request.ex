5d192a6ba533a7b7e6d08306864970171759d9e9
Sonny Scroggin <scrogson@gmail.com>
2015-10-24 00:52:58 -0500

    Simplify the Request module

--- lib/oauth2/request.ex
+++ lib/oauth2/request.ex
@@ -3,15 +3,19 @@ defmodule OAuth2.Request do
 
   use HTTPoison.Base
 
+  import OAuth2.Util
+
   alias OAuth2.Error
   alias OAuth2.Response
 
   def request(method, url, body \\ "", headers \\ [], opts \\ []) do
-    content_type = OAuth2.Util.content_type(headers)
-    url = process_url(to_string(url))
-    body = process_request_body(body, content_type)
-    headers = process_request_headers(headers, content_type)
-    :hackney.request(method, url, headers, body, opts) |> do_request
+    body = process_request_body(body, content_type(headers))
+    case super(method, url, body, headers, opts) do
+      {:ok, %HTTPoison.Response{status_code: status, headers: headers, body: body}} ->
+        {:ok, Response.new(status, headers, body)}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:ok, %Error{reason: reason}}
+    end
   end
 
   def request!(method, url, body \\ "", headers \\ [], options \\ []) do
@@ -25,22 +29,7 @@ defmodule OAuth2.Request do
     [{"Accept", content_type} | headers]
 
   defp process_request_body("", _), do: ""
-  defp process_request_body(body, "application/json"), do:
-    Poison.encode!(body)
+  defp process_request_body(body, "application/json"), do: Poison.encode!(body)
   defp process_request_body(body, "application/x-www-form-urlencoded"), do:
     Plug.Conn.Query.encode(body)
-
-  defp do_request({:ok, status_code, headers, _client}) when status_code in [204, 304], do:
-    {:ok, Response.new(status_code, headers, "")}
-  defp do_request({:ok, status_code, headers}), do:
-    {:ok, Response.new(status_code, headers, "")}
-  defp do_request({:ok, status_code, headers, client}), do:
-    handle_request_body(:hackney.body(client), status_code, headers)
-  defp do_request({:error, reason}), do:
-    {:error, %Error{reason: reason}}
-
-  defp handle_request_body({:ok, body}, status_code, headers), do:
-    {:ok, Response.new(status_code, headers, body)}
-  defp handle_request_body({:error, reason}, _status_code, _headers), do:
-    {:error, %Error{reason: reason}}
 end
