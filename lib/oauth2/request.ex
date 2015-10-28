155ef1d3461704f30e75199956fbd70d7a269f62
Sonny Scroggin <scrogson@gmail.com>
2015-10-27 19:26:37 -0500

    Don't include this module in docs.
    It’s not meant to be used by users

--- lib/oauth2/request.ex
+++ lib/oauth2/request.ex
@@ -1,8 +1,5 @@
 defmodule OAuth2.Request do
-  @moduledoc """
-    This module is used for making HTTP requests. You should never use this
-    directly instead use `OAuth2.AccessToken`
-  """
+  @moduledoc false
 
   use HTTPoison.Base
 
@@ -11,10 +8,6 @@ defmodule OAuth2.Request do
   alias OAuth2.Error
   alias OAuth2.Response
 
-  @type method  :: :post | :get | :put | :delete | atom
-  @type url     :: binary
-
-  @spec request(method, url, binary, Client.headers, []) :: {:ok, OAuth2.Response.t} | {:error, OAuth2.Error.t}
   def request(method, url, body \\ "", headers \\ [], opts \\ []) do
     content_type = content_type(headers)
     body = process_request_body(body, content_type)
@@ -28,7 +21,6 @@ defmodule OAuth2.Request do
     end
   end
 
-  @spec request(method, url, binary, Client.headers, []) :: OAuth2.Response.t | OAuth2.Error.t
   def request!(method, url, body \\ "", headers \\ [], options \\ []) do
     case request(method, url, body, headers, options) do
       {:ok, response} -> response
