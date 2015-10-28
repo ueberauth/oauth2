6712062b14d8776de3a6e63e17ca65e1959d2732
Sonny Scroggin <scrogson@gmail.com>
2015-10-28 08:25:10 -0500

    whitespace

--- test/oauth2/client_test.exs
+++ test/oauth2/client_test.exs
@@ -7,7 +7,7 @@ defmodule OAuth2.ClientTest do
 
   test "authorize_url" do
     client = OAuth2.new(@opts)
-  {_client, url} = authorize_url(client)
+    {_client, url} = authorize_url(client)
     assert url == "http://localhost/oauth/authorize?client_id=&redirect_uri=http%3A%2F%2Flocalhost%2Fauth%2Fcallback&response_type=code"
   end
 
