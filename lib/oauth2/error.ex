4422b63067a1dd57c5c9840482337ee9d86fb6ba
Sonny Scroggin <scrogson@gmail.com>
2015-11-03 00:20:27 -0600

    Improve error for :econnrefused

--- lib/oauth2/error.ex
+++ lib/oauth2/error.ex
@@ -1,5 +1,6 @@
 defmodule OAuth2.Error do
   defexception [:reason]
+  def message(%__MODULE__{reason: :econnrefused}), do: "Connection refused"
   def message(%__MODULE__{reason: reason}) when is_binary(reason), do: reason
   def message(%__MODULE__{reason: reason}), do: inspect reason
 end
