d8bbe5257c3f01afe758c9125cb802f2cd1714a3
Fajar Maulana Firdaus <fajarmf@salestock.id>
2016-04-06 22:41:27 +0700

    use fajarmf's httpoison which use hackney 1.6.0

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule OAuth2.Mixfile do
   end
 
   defp deps do
-    [{:httpoison, "~> 0.7"},
+    [{:httpoison, git: "https://github.com/fajarmf/httpoison"},
      {:poison, "~> 1.3 or ~> 2.0"},
      {:mimetype_parser, "~> 0.1"},
 
