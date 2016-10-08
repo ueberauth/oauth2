249e2dc058460ae46d1e62ed2a83a3bf01fdf800
Sonny Scroggin <scrogson@gmail.com>
2016-10-08 00:18:38 -0500

    set logger to info since bypass doesn't seem to work right

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :bypass, enable_debug_log: false
+config :logger, level: :info
 
 config :oauth2,
   client_id: "0bee1126b1a1381d9cab60bcd52349484451808a", # first commit sha of this library
