7970ce68fe9d8c853f5781fbaa4d00727f3b7d6f
Simon Belluzzo <simonalpha@users.noreply.github.com>
2017-01-25 18:35:21 +1100

    Fix handling of Content-Types with multiple params (#87)
    Microsoft's Azure AD v2.0 token endpoint responds with a Content-Type
    containing multiple parameters, but the previous code only processes
    cases with a content type, with or without a single parameter.

--- test/oauth2/util_test.exs
+++ test/oauth2/util_test.exs
@@ -7,6 +7,7 @@ defmodule OAuth2.UtilTest do
     assert "application/json" == Util.content_type([])
     assert "application/vnd.api+json" == Util.content_type([{"content-type", "application/vnd.api+json"}])
     assert "application/xml" == Util.content_type([{"content-type", "application/xml; version=1.0"}])
+    assert "application/json" == Util.content_type([{"content-type", "application/json;param;param"}])
 
     assert_raise OAuth2.Error, fn ->
       Util.content_type([{"content-type", "trash; trash"}])
