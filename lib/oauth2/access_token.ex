3a108f6ec4d82d47e106eb33a64517fa3f979706
Michael Howell <michael@notriddle.com>
2017-02-21 12:51:13 -0700

    Allow AccessToken.refresh_token to be nil
    The system supports it, so not including it in the typespec causes
    dialyzer to produce unreachable code errors.

--- lib/oauth2/access_token.ex
+++ lib/oauth2/access_token.ex
@@ -15,7 +15,7 @@ defmodule OAuth2.AccessToken do
   @standard ["access_token", "refresh_token", "expires_in", "token_type"]
 
   @type access_token  :: binary
-  @type refresh_token :: binary
+  @type refresh_token :: binary | nil
   @type expires_at    :: integer
   @type token_type    :: binary
   @type other_params  :: %{}
