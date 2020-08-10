f9acd69665e023d16568b5985fecc9b9c380d9ce
Gigitsu <gigitsu.23@gmail.com>
2020-08-10 12:14:57 +0200

    Updated hackney

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule OAuth2.Mixfile do
 
   defp deps do
     [
-      {:hackney, "~> 1.13"},
+      {:hackney, "~> 1.16"},
 
       # Test dependencies
       {:jason, "~> 1.0", only: [:dev, :test]},
