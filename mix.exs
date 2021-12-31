467463761000d415bce28683c6837986f09f01ed
ritou <ritou.06@gmail.com>
2021-12-31 23:27:47 +0900

    add jose to deps

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule OAuth2.Mixfile do
   defp deps do
     [
       {:hackney, "~> 1.13"},
+      {:jose, "~> 1.11"},
 
       # Test dependencies
       {:jason, "~> 1.0", only: [:dev, :test]},
