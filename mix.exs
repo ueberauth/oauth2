0d7e6711d0e84298ae5a7b20180a44b0dbedeb23
Olafur Arason <olafura@olafura.com>
2022-12-28 15:08:36 +0100

    update tesla to fix httpc

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule OAuth2.Mixfile do
 
   defp deps do
     [
-      {:tesla, "~> 1.4"},
+      {:tesla, "~> 1.5"},
 
       # Test dependencies
       {:hackney, "~> 1.17", only: [:dev, :test]},
